316bb6514c8c1549d3bedf804f1ff225ce9253dc
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2024-01-29 12:07:51 +0200

    created content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { ThemeProvider } from '@mui/material';
 import theme from './styles';
 import Header from './components/Header';
+import Banner from './components/Banner';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Header/>
+      <Banner/>
+      <Content/>
+      <Footer/>
     </ThemeProvider>
-    );
-}
+  );
+};
 
 export default App;
